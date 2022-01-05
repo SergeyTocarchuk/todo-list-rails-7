1fa0e18bb3eba3007d9ab07f14b22363817d946d
Serhii T <tocarchuk@gmail.com>
2022-01-05 15:31:33 +0200

    Home page displays current User Todo list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-  
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(user)
+    lists_path
+  end
 end
