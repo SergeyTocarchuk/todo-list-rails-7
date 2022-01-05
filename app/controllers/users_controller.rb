0d194f33efacea390271ac7d98ba12b72943a065
Serhii T <tocarchuk@gmail.com>
2022-01-05 12:20:18 +0200

    Admin changes role of a User: admin or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,28 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:email, :admin)
+  end
+
 end
