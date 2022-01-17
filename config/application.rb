19f97fc7245fc3af42d1398cd7f0c864a48c3cb2
Serhii T <tocarchuk@gmail.com>
2022-01-17 11:00:30 +0200

    Change config/application.rb for deployment

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module TodoList
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.path <<< Rails.root.join('app', 'assets', 'images', 'shared')
   end
 end
