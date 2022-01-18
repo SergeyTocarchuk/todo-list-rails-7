1a1d2026dad98d04dddb962cb1172df37c524cee
Serhii T <tocarchuk@gmail.com>
2022-01-18 20:57:30 +0200

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :user_lists, dependent: :destroy
-  has_many :lists, through: :user_lists
+  has_many :user_lists
+  has_many :lists, through: :user_lists, dependent: :destroy
 end
