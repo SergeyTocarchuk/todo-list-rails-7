156e750acd6ecc7cd06932ace86a182f1bc4ae07
andrei <andrew.motinga@gmail.com>
2022-01-29 12:11:33 -0800

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 user = User.create(email: 'user@gmail.com', password: 'password', password_confirmation: 'password')
+andrei = User.create(email: 'andrei@gmail.com', password: 'password', password_confirmation: 'password')
+serghei = User.create(email: 'serghei@gmail.com', password: 'password', password_confirmation: 'password')
 admin = User.create(email: 'admin@gmail.com', password: 'password', password_confirmation: 'password', admin: true)
 
 
@@ -8,6 +10,9 @@ user.lists << groceries
 user.lists << work
 
 
-1000.times do
-  List.create(name: Faker::Lorem.sentence )
-end
+andrei.lists << List.create(name: 'Andreis Groceries')
+serghei.lists << List.create(name: 'Sergehis Groceries')
+
+# 1000.times do
+#   List.create(name: Faker::Lorem.sentence )
+# end
