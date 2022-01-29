5ec207da8d971c23cc1a27d7c5dd13ff29198484
andrei <andrew.motinga@gmail.com>
2022-01-29 12:41:32 -0800

    limit options to only possible ones

--- app/models/user_list.rb
+++ app/models/user_list.rb
@@ -1,4 +1,9 @@
 class UserList < ApplicationRecord
   belongs_to :user
   belongs_to :list
+
+  def self.sharable_user_ids(list_id)
+    ids = UserList.where(list_id: list_id).pluck(:user_id)
+    User.where.not(id: ids)
+  end
 end
