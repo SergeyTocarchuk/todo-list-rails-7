593f4be02dc799539c9fc1fcceeb6535cab861e8
Serhii T <tocarchuk@gmail.com>
2022-02-02 12:50:07 +0200

    Style List#Index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
 
   has_many :user_lists, dependent: :destroy
   has_many :lists, through: :user_lists
+
+  def find_day_of_week
+    DateTime.now.strftime("%A")
+  end
+
+  def find_month
+    DateTime.now.strftime("%B")
+  end
+
+  def find_date
+    DateTime.now.strftime("%d")
+  end
+
+  def find_year
+    DateTime.now.strftime("%Y")
+  end
 end
